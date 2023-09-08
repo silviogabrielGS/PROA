be76b62217b201d002aa9ba9c92b1025892d681f
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-09-08 16:23:38 -0300

    finalizando projeto pepsi

---
+++ Interfaces HTML e CSS/Projetos/Projeto10/js/script.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything
+}
+
+function changeBackground(color) {
+    const backColor = document.querySelector('.sec');
+    backColor.style.background = color
+}
