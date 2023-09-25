9d7ab2bfc899562797bd3e8f2545de7ab35b9df9
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-09-25 13:07:54 -0300

    exercícios feitos durante a aula de javascript

---
+++ Javascript/exercicios/js/script05.js
@@ -0,0 +1,21 @@
+//Escreva um programa que calcule a soma dos números de 1 a 100 //usando o loop "do-while".
+
+ 
+
+let contadora=1;
+
+let somatoria=0;
+
+ 
+
+do {
+
+    somatoria=somatoria+contadora;
+
+    contadora++;
+
+} while (contadora<=100);
+
+ 
+
+alert(somatoria)
---
+++ Javascript/exercicios/js/script06.js
@@ -0,0 +1,33 @@
+
+
+let primeiroNumero=1;
+
+let segundoNumero=1;
+
+let fibonacci=0;
+
+let contadora=1;
+
+ 
+
+do {
+
+alert(primeiroNumero)
+
+fibonacci=primeiroNumero+segundoNumero; 3-5-8-13-21-34
+
+primeiroNumero=segundoNumero;
+
+segundoNumero=fibonacci
+
+contadora++;
+
+} while (contadora<21);
+
+   
+
+ 
+
+ 
+
+ 
