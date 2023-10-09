22547b7ca4095dc35f8b21aa98851789b3c6b2ef
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-10-09 13:45:42 -0300

    adicionando estrutura feita em aula

--- projeto01/src/App.jsx
+++ projeto01/src/App.jsx
@@ -1,12 +1,12 @@
 //imports
 //funções
 //exportar
-
- 
+import "./App.css"
 
 function App() {
   return(
     <>
+      <h1> olá mundo</h1>
       <p>Hello Word</p>
     </>
   )
--- projeto01/src/main.jsx
+++ projeto01/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App/>
+  </React.StrictMode>
 )
+
+
+
+
+
