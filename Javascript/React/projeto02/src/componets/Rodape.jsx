e2045b3d63c51cb500b7cbe1caa3802106fded56
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-10-11 15:13:55 -0300

    Aprendendo React

---
+++ Javascript/React/projeto02/src/componets/Rodape.jsx
@@ -0,0 +1,10 @@
+function Rodape(props) {
+    return (
+        <div className="footer">
+            <p>loja de informática do Obama {props.nome} </p>
+            <p>Todos os direitos reservados </p>
+        </div>
+    )
+}
+
+export default Rodape
