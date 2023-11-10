3b84e3593978bc488cc2c175eb98b1fb06709065
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2023-11-10 17:46:35 -0300

    atualizando o menu

--- Javascript/React/projeto07/src/components/Menu.jsx
+++ Javascript/React/projeto07/src/components/Menu.jsx
@@ -1,40 +1,55 @@
 import './Menu.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/js/index.esm'
-import logoMenu from "../assets/logoMenu.png"
+import logoMenu from "../assets/logo-pequena-verde.svg"
 
 function Menu() {
     return (
         <>
-             <nav className="navbar navbar-expand-md navbar-light bg-light">
-                <div className="container-fluid justify-content-between">
+             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid ">
 
                         <img src={logoMenu} alt="" width="150px" height="100px" style={{margin:"0px", padding:"0px"}} />
-    
-
-                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="true" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                    <div className="collapse navbar-collapse justify-content-center text-center" id="navbarNav">
+                     <div className='d-flex justify-content-center'>
+                        <a className='d-block d-lg-none ' href="#" style={{backgroundColor:"#157E41", width:"80px" ,height:"80px", borderRadius:"50%"}}/> 
+                     </div>
+                            <div className='d-none d-lg-block'></div>
                         <ul className="navbar-nav">
-                            <li className="nav-item Limpay-teste">
-                                <a className="nav-link active " aria-current="page" href="#">Home</a>
+                            <li className="nav-item Limpay-teste mx-3">
+                                <a className="nav-link active " aria-current="page" href="#">Inicio</a>
                             </li>
-                            <li className="nav-item Limpay-teste">
-                                <a className="nav-link" href="#">Features</a>
+                            <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle mx-3" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Carteira
+                            </a>
+                                <ul className="dropdown-menu text-center " aria-labelledby="navbarDropdownMenuLink" id="carteira" style={{backgroundColor:"#157E41"}}>
+                                    <li ><a className="dropdown-item " href="#" style={{color:"#fff", backgroundColor:"#157E41"}} >Carteira</a></li>
+                                    <li><hr className="dropdown-divisor" style={{color:"#fff", border:"solid 1px #fff"}} /></li>
+                                    <li><a className="dropdown-item" href="#"  style={{color:"#fff", backgroundColor:"#157E41"}} >Saldo em conta R$250,00</a></li>
+                                    <div className="d-flex justify-content-center">
+                                        <button type='button' className="dropdown-item btn btn-link btn-sm " style={{backgroundColor:"#157E41"}}><a  href="#">Mais informações</a></button>
+                                    </div>
+                                </ul>
                             </li>
-                            <li className="nav-item Limpay-teste">
-                                <a className="nav-link" href="#">Pricing</a>
+                            <li className="nav-item Limpay-teste mx-3">
+                                <a className="nav-link" href="#">Educacional</a>
                             </li>
-                            <li className="nav-item Limpay-teste">
-                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <li className="nav-item Limpay-teste mx-3">
+                                <a className="nav-link" href="#">Quem Somos</a>
+                            </li>
+                            <li className="nav-item Limpay-teste mx-3">
+                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Suporte</a>
                             </li>
                         </ul>
                     </div>
 
-                    <a className='d-none d-md-block' href="#" style={{backgroundColor:"#157E41", width:"80px" ,height:"80px", borderRadius:"50%"}}>
-                    <div className='d-none d-md-block'></div>
+                    <a className='d-none d-lg-block ' href="#" style={{backgroundColor:"#157E41", width:"80px" ,height:"80px", borderRadius:"50%"}}>
+                    <div className='d-none d-lg-block'></div>
                     </a>
                 </div>
             </nav>  
